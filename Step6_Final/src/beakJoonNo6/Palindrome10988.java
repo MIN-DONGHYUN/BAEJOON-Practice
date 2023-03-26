b08110a0359480baa5dc0cc9e2e68b3ce3f119b4
MIN-DONGHYUN <mdh2057@naver.com>
2023-03-26 19:36:38 +0900

    2023.03.26 BAEKJOON STEP6 Exclude the last question

--- Step6_Final/src/beakJoonNo6/Palindrome10988.java
+++ Step6_Final/src/beakJoonNo6/Palindrome10988.java
@@ -1,10 +1,8 @@
 package beakJoonNo6;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 
 public class Palindrome10988 {
 
@@ -12,50 +10,27 @@ public class Palindrome10988 {
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));	//버퍼 리더를 사용해보자 
 		// 버퍼 리더선언 오리발, (new InputStreamReader(system.in 을 사용하여 입력을 받아온다.))
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
+		int cnt = 1;   // 단어가 1일때를 감안해서 초기값 1로 설정
 		
-		int cnt = 0;
-		boolean gogo = false;
-		
-		String str = br.readLine();
+		String str = br.readLine();				// 키보드로써 읽어온다
 
 		
-		if(1 <= str.length() && str.length() <= 100)
+		for(int i = 0; i < str.length()/2; i++)			// 단어의 길이의 반만큼 돌린다. 홀수일경우 가운데 글자는 비교 할 필요 없으니 가능
 		{
-			for(int z = 0 ; z < str.length(); z++)
-			{
-				if('a' <= str.charAt(z) && str.charAt(z) <= 'z')
-				{
-					gogo = true;
-				}
-				else {
-					gogo = false;
-					break;
-				}
-				
+			if(str.charAt(i) != str.charAt(str.length() - i -1 ))	// 비교하기 예시 밑줄 
+			{                //단어길이 5  반복회수  2  단어  level  비교 인덱스 번호   01   43
+							 //단어길이 4  반복회수  2  단어  noon   비교 인덱스 번호   01   32
+				cnt = 0;
+				break;
 			}
-			
-			if( gogo == true)
-			{
-				for(int i = 0; i < str.length()/2; i++)
-				{
-					if(str.charAt(i) != str.charAt(str.length() - i -1 ))
-					{                // 5    2    level    01   43
-						cnt = 0;
-						break;
-					}
-					else {
-						cnt = 1;
-					}
-				}
-			}	
+			else {
+				cnt = 1;
+			}
 		}
-		
-		bw.write(cnt + "\n");
-		bw.flush();
-		bw.close();
-		bw.close();
+		System.out.println(cnt);
+	
+		br.close();
 	}
 
 }
